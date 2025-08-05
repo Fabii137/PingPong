b49b187060975a113bc530e974507ffe5368b1ae
Fabian KRSTIC <Fabian.KRSTIC@bachmann.info>
2025-08-05 11:01:32 +0200

    added gameOver, score, simple game logic

--- include/States/GameState.hpp
+++ include/States/GameState.hpp
@@ -4,6 +4,7 @@
 #include "States/State.hpp"
 #include "Entities/Player.hpp"
 #include "Entities/Ball.hpp"
+#include <format>
 
 class GameState : public State {
 public:
@@ -13,12 +14,30 @@ public:
 	void update(const float& dt);
 	void render(sf::RenderTarget* target);
 private:
+	bool m_GameOver = false;
+
 	std::unique_ptr<Player> m_Player1;
 	std::unique_ptr<Player> m_Player2;
 	std::unique_ptr<Ball> m_Ball;
-private:
-	void updateInput(const float& dt);
 
+	int m_ScorePlayer1 = 0;
+	int m_ScorePlayer2 = 0;
+	const int m_MaxScore = 2;
+
+	sf::Font m_Font;
+	std::unique_ptr<sf::Text> m_ScoreText1;
+	std::unique_ptr<sf::Text> m_ScoreText2;
+	std::unique_ptr<sf::Text> m_WonText;
+	std::unique_ptr<sf::Text> m_RestartText;
+private:
+	void initFont();
+	void initText();
+	void initGameObjects();
+	void handleGameplayInput(const float& dt);
+	void handleGameOverInput();
+	void updateInput(const float& dt);
+	void checkWin();
+	void resetRound();
 };
 
 #endif /* GAMESTATE_HPP */
--- src/States/gameState.cpp
+++ src/States/gameState.cpp
@@ -6,14 +6,10 @@
 
 GameState::GameState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<State*>* states): State(window, supportedKeys, states) {
 	initKeybinds(std::string(RESOURCES_PATH) + "configs/gamestate_keybinds.ini");
-	m_Player1 = std::make_unique<Player>(*m_Window);
-	m_Player1->setPosition(sf::Vector2f(100.f, 540.f));
+	initFont();
+	initText();
+	initGameObjects();
 
-	m_Player2 = std::make_unique<Player>(*m_Window);
-	m_Player2->setPosition(sf::Vector2f(1820.f, 540.f));
-
-	m_Ball = std::make_unique<Ball>(*m_Player1->getShape(), *m_Player2->getShape(), *m_Window);
-	m_Ball->setPosition(sf::Vector2f(960.f, 540.f));
 	m_Ball->start(); // start moving ball
 }
 
@@ -26,6 +22,7 @@ void GameState::update(const float& dt) {
 	updateInput(dt);
 
 	m_Ball->update(dt);
+	checkWin();
 }
 
 void GameState::render(sf::RenderTarget* target) {
@@ -37,10 +34,53 @@ void GameState::render(sf::RenderTarget* target) {
 	m_Player2->render(target);
 	m_Ball->render(target);
 
+	target->draw(*m_ScoreText1);
+	target->draw(*m_ScoreText2);
+
+	if (m_GameOver) {
+		target->draw(*m_WonText);
+		target->draw(*m_RestartText);
+	}
+	
+
 }
 
-void GameState::updateInput(const float& dt) {
-	
+void GameState::initFont() {
+	if (!m_Font.openFromFile(std::string(RESOURCES_PATH) + "fonts/Dosis-Light.ttf")) {
+		throw("ERROR::GAMESTATE::COULD NOT LOAD FONT");
+	}
+}
+
+void GameState::initText() {
+	m_ScoreText1 = std::make_unique<sf::Text>(m_Font, "0", 50);
+	m_ScoreText1->setPosition(sf::Vector2f(200.f, 100.f));
+	m_ScoreText1->setFillColor(sf::Color::White);
+
+	m_ScoreText2 = std::make_unique<sf::Text>(m_Font, "0", 50);
+	m_ScoreText2->setPosition(sf::Vector2f(1700.f, 100.f));
+	m_ScoreText2->setFillColor(sf::Color::White);
+
+	m_WonText = std::make_unique<sf::Text>(m_Font, "", 50);
+	m_WonText->setPosition(sf::Vector2f(800.f, 200.f));
+	m_WonText->setFillColor(sf::Color::White);
+
+	m_RestartText = std::make_unique<sf::Text>(m_Font, "", 40);
+	m_RestartText->setPosition(sf::Vector2f(800.f, 400.f));
+	m_RestartText->setFillColor(sf::Color::White);
+}
+
+void GameState::initGameObjects() {
+	m_Player1 = std::make_unique<Player>(*m_Window);
+	m_Player1->setPosition(sf::Vector2f(100.f, 540.f));
+
+	m_Player2 = std::make_unique<Player>(*m_Window);
+	m_Player2->setPosition(sf::Vector2f(1820.f, 540.f));
+
+	m_Ball = std::make_unique<Ball>(*m_Player1->getShape(), *m_Player2->getShape(), *m_Window);
+	m_Ball->setPosition(sf::Vector2f(960.f, 540.f));
+}
+
+void GameState::handleGameplayInput(const float& dt) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(m_Keybinds["P1_MOVE_UP"]))) {
 		m_Player1->move(dt, -1.f);
 	}
@@ -53,10 +93,66 @@ void GameState::updateInput(const float& dt) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(m_Keybinds["P2_MOVE_DOWN"]))) {
 		m_Player2->move(dt, 1.f);
 	}
+}
+
+void GameState::handleGameOverInput() {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(sf::Keyboard::Key::Space))) {
+		m_ScorePlayer1 = 0;
+		m_ScoreText1->setString("0");
+
+		m_ScorePlayer2 = 0;
+		m_ScoreText2->setString("0");
+
+		m_WonText->setString("");
+		m_RestartText->setString("");
+		m_GameOver = false;
+		resetRound();
+	}
+}
+
+void GameState::updateInput(const float& dt) {
+	
+	if (!m_GameOver) {
+		handleGameplayInput(dt);
+	}
+	else {
+		handleGameOverInput();
+	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(m_Keybinds["CLOSE"]))) {
 		endState();
 	}
+}
 
-	
+void GameState::checkWin() {
+	sf::Vector2f ballPos = m_Ball->getShape()->getPosition();
+	sf::Vector2u windowSize = m_Window->getSize();
+
+	if (ballPos.x < 0) {
+		m_ScorePlayer2++;
+		m_ScoreText2->setString(std::to_string(m_ScorePlayer2));
+		resetRound();
+	}
+	else if (ballPos.x > windowSize.x) {
+		m_ScorePlayer1++;
+		m_ScoreText1->setString(std::to_string(m_ScorePlayer1));
+		resetRound();
+	}
+
+	if (m_ScorePlayer1 >= m_MaxScore || m_ScorePlayer2 >= m_MaxScore) {
+		std::string wonText = std::format("Player {} won!", (m_ScorePlayer1 >= m_MaxScore ? "1" : "2"));
+		m_WonText->setString(wonText);
+		m_RestartText->setString("Press Space to play again!");
+		m_GameOver = true;
+		m_Ball->stop();
+	}
+}
+
+void GameState::resetRound() {
+	m_Player1->setPosition(sf::Vector2f(100.f, 540.f));
+	m_Player2->setPosition(sf::Vector2f(1820.f, 540.f));
+
+	m_Ball->setPosition(sf::Vector2f(960.f, 540.f));
+	m_Ball->reset();
+	m_Ball->start();
 }
